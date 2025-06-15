d2d6ab234c9567a2bb7540b3a56552488b998a7a
jieun4510@sju.ac.kr <jieun4510@sju.ac.kr>
2025-06-15 10:00:47 +0900

    first commit

---
+++ src/main/java/com/example/period/PeriodApplication.java
@@ -0,0 +1,13 @@
+package com.example.period;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeriodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeriodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/period/PeriodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.period;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeriodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
